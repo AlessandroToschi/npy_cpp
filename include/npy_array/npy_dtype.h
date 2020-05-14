038c2ed2f88b1f58732723af65643bbcfe347a81
Alessandro Toschi <ialessandrotoschi@gmail.com>
2020-05-14 17:18:26 +0200

    finished commet header npy dtype

--- include/npy_array/npy_dtype.h
+++ include/npy_array/npy_dtype.h
@@ -84,7 +84,7 @@ public:
      * The null dtype object has the following invariants:
      * kind = npy_dtype_kind::unknown
      * item size = 0
-     * byte order = npy_endianess::not_applicable.
+     * byte order = npy_endianess::not_applicable.>
      */
     npy_dtype() noexcept;
 
@@ -259,29 +259,123 @@ public:
      */
     static npy_dtype from_string(const std::string& dtype_string) noexcept;
 
+    /**
+     * @brief Return a 8-bit boolean value, equivalent to a bool C++ type.
+     * 
+     * @return npy_dtype boolean dtype.
+     */
     static npy_dtype bool_8() noexcept;
+    /**
+     * @brief Return a 8-bit signed integer value, equivalent to a char and int8_t C++ type.
+     * 
+     * @return npy_dtype 8-bit int dtype.
+     */
     static npy_dtype int_8() noexcept;
+    /**
+     * @brief Return a 16-bit signed integer value, equivalent to a shprt and int16_t C++ type.
+     * 
+     * @return npy_dtype 16-bit int dtype.
+     */
     static npy_dtype int_16() noexcept;
+    /**
+     * @brief Return a 32-bit signed integer value, equivalent to a int and int32_t C++ type.
+     * 
+     * @return npy_dtype 32-bit int dtype.
+     */
     static npy_dtype int_32() noexcept;
+    /**
+     * @brief Return a 64-bit signed integer value, equivalent to a long and int64_t C++ type.
+     * 
+     * @return npy_dtype 64-bit int dtype.
+     */
     static npy_dtype int_64() noexcept;
+    /**
+     * @brief Return a 8-bit unsigned integer value, equivalent to a unsigned char and uint8_t C++ type.
+     * 
+     * @return npy_dtype 8-bit unsigned int dtype.
+     */
     static npy_dtype uint_8() noexcept;
+    /**
+     * @brief Return a 16-bit unsigned integer value, equivalent to a unsigned short and uint16_t C++ type.
+     * 
+     * @return npy_dtype 16-bit unsigned int dtype.
+     */
     static npy_dtype uint_16() noexcept;
+    /**
+     * @brief Return a 32-bit unsigned integer value, equivalent to a unsigned int and uint32_t C++ type.
+     * 
+     * @return npy_dtype 32-bit unsigned int dtype.
+     */
     static npy_dtype uint_32() noexcept;
+    /**
+     * @brief Return a 64-bit unsigned integer value, equivalent to a unsigned long and uint64_t C++ type.
+     * 
+     * @return npy_dtype 64-bit unsigned int dtype.
+     */
     static npy_dtype uint_64() noexcept;
+    /**
+     * @brief Return a 32-bit floating value, equivalent to a float C++ type.
+     * 
+     * @return npy_dtype 32-bit floating value dtype.
+     */
     static npy_dtype float_32() noexcept;
+    /**
+     * @brief Return a 64-bit floating value, equivalent to a double C++ type.
+     * 
+     * @return npy_dtype 64-bit floating value dtype.
+     */
     static npy_dtype float_64() noexcept;
+    /**
+     * @brief Return a 128-bit floating value, equivalent to a long double C++ type.
+     * 
+     * @return npy_dtype 128-bit floating value dtype.
+     */
     static npy_dtype float_128() noexcept;
+    /**
+     * @brief Return a 64-bit complex, equivalent to a std::complex<float> C++ type.
+     * 
+     * @return npy_dtype 64-bit complex dtype.
+     */
     static npy_dtype complex_64() noexcept;
+    /**
+     * @brief Return a 128-bit complex, equivalent to a std::complex<double> C++ type.
+     * 
+     * @return npy_dtype 128-bit complex dtype.
+     */
     static npy_dtype complex_128() noexcept;
+    /**
+     * @brief Return a 256-bit complex, equivalent to a std::complex<long double> C++ type.
+     * 
+     * @return npy_dtype 256-bit complex dtype.
+     */
     static npy_dtype complex_256() noexcept;    
+    /**
+     * @brief Return a null dtype.
+     * 
+     * The null dtype object has the following invariants:
+     * kind = npy_dtype_kind::unknown
+     * item size = 0
+     * byte order = npy_endianess::not_applicable.
+     * 
+     * @return npy_dtype null dtype.
+     */
     static npy_dtype null() noexcept;
     
 private:
+    /**
+     * @brief Private constructor that construct a new dtype with the user provided inputs.
+     * 
+     * It is used by the static methods to create the implemeted dtypes.
+     * 
+     * @param kind dtype's kind.
+     * @param item_size dtype size in bytes.
+     * @param byte_order the endianess of the dtype.
+     */
     npy_dtype(npy_dtype_kind kind, size_t item_size, npy_endianness byte_order=get_endianess()) noexcept;
 
-    npy_dtype_kind _kind;
-    size_t _item_size;
-    npy_endianness _byte_order;
+    npy_dtype_kind _kind; // The private dtype's kind.
+    size_t _item_size; // The private item size in bytes.
+    npy_endianness _byte_order; // The private dtype's byte order.
 };
 
 #endif /* D4AB99B8_10DA_46D7_9CFA_559B460F6ABA */
